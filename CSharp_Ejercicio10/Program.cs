de89a69b9c234438bf4cb407f8249ad33b0b705a
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:53:27 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Pide 10 números por consola, muestra el resultado por consola de sumar los pares y restarle los impares.
+int sumaPares = 0;
+int restaImpares = 0;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Ingrese un número:");
+    int numero121 = Convert.ToInt32(Console.ReadLine());
+    if (numero121 % 2 == 0)
+    {
+        sumaPares += numero121;
+    }
+    else
+    {
+        restaImpares -= numero121;
+    }
+}
+Console.WriteLine("La suma de los números pares es: " + sumaPares);
+Console.WriteLine("La resta de los números impares es: " + restaImpares);
